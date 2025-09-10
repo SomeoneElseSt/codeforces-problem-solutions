fe698ef950cd198d83be852d74ad16119756c3d1
Stiven <Strnmrtnz@icloud.com>
2025-09-10 11:12:59 +0900

    feat: fixed 0 edge case, var names, etc

--- next_round.py
+++ next_round.py
@@ -2,10 +2,17 @@ n, k = map(int, input().split())
 
 scores = list(map(int, input().split()))
 
-new_participans = 0
+new_participants = 0
 
-for score in scores:
-    if score >= k:
-        new_participants += 1
+k_th_score = scores[k - 1] 
+
+if k_th_score == 0:
+    for score in scores:
+        if score > 0:
+            new_participants += 1
+else:
+    for score in scores:
+        if score >= k_th_score:
+            new_participants += 1
 
 print(new_participants)
