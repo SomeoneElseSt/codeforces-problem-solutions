e2b5e4506e06b01b21447e8ea2c0067e2cd36191
Stiven <Strnmrtnz@icloud.com>
2025-09-11 11:17:24 +0900

    feat: refactored code to use a scale factor function instead of hardcoded values

--- bear_and_big_brother.py
+++ bear_and_big_brother.py
@@ -1,18 +1,29 @@
+SCALE_FACTOR_LIMAK = 3 
+SCALE_FACTOR_VIVEK = 2
+
+def find_scale_factor_difference(a: int, b: int, scale_factor_a: int, scale_factor_b: int, ascending_order: bool):
+    
+    difference = 0
+
+    if not isinstance(a, int) or not isinstance(b, int):
+        print(f"Number input {a} {b} is not an integer and can not be processed")
+
+    if not isinstance(scale_factor_a, int) or not isinstance(scale_factor_b, int):
+        print(f"Scale factor input {scale_factor_a} {scale_factor_b} is not an integer and can not be processed")
+
+    while a <= b if ascending_order else a >= b:
+        difference += 1
+        a = a * scale_factor_a
+        b = b * scale_factor_b
+
+    return difference
+
 a, b = input().split()
 
 a = int(a)
 b = int(b)
 
-years_counter = 0
-
 if a == b:
     print("1")
 else: 
-    while a <= b:
-        years_counter += 1 
-        a = a * 3
-        b = b * 2
-        print(years_counter)
-
-
-
+    print(find_scale_factor_difference(a, b, SCALE_FACTOR_LIMAK, SCALE_FACTOR_VIVEK, True))
