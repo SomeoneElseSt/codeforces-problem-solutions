03ca40ccdbc9e17e8c279fb5308b95f4920c7e1f
Stiven <Strnmrtnz@icloud.com>
2025-09-12 10:06:35 -0700

    feat: solution to stones on the table using a while loop

--- stones_on_the_table.py
+++ stones_on_the_table.py
@@ -1,3 +1,17 @@
+n = int(input())
+stones = list(input().strip())
 
-# Iter over an array with pointers and if the pointers meet with each other (the two elements are the same) eliminate the one in the right and add 1 to the steps counter. 
+left_index = 0
+right_index = 1
 
+rocks_counter = 0
+
+while right_index < len(stones):
+    if stones[left_index] == stones[right_index]:
+        rocks_counter += 1
+        stones.pop(right_index)
+    else:
+        right_index += 1
+        left_index += 1
+
+print(rocks_counter)
