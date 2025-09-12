4a057b253d9415e96df95aac828db551d1bef422
Stiven <Strnmrtnz@icloud.com>
2025-09-13 03:30:01 +0900

    feat: fixed logic comparator and modified constant

--- elephant.py
+++ elephant.py
@@ -3,13 +3,13 @@ target_position = int(input())
 starting_position = 0
 steps_count = 0 
 
-available_steps = [1, 2, 3, 4, 5]
+AVAILABLE_STEPS = [1, 2, 3, 4, 5]
 
-if target_position in available_steps:
+if target_position in AVAILABLE_STEPS:
     print("1")
 else: 
     while starting_position != target_position:
         steps_count += 1 
-        starting_position += max(step for step in available_steps if step < starting_position - starting_position)
+        starting_position += max(step for step in AVAILABLE_STEPS if step <= target_position - starting_position)
 
     print(steps_count)
