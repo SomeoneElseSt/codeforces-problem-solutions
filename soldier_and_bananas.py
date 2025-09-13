b19e8401675e6f4e32266219e219b2a942e16088
Stiven <Strnmrtnz@icloud.com>
2025-09-13 14:39:37 +0900

    fixed some logic when no money needs borrowing

--- soldier_and_bananas.py
+++ soldier_and_bananas.py
@@ -1,25 +1,12 @@
-# 
-# w = bananas
-# k = price of banana, doubles at each  iter 
-# n = dollars
-
-
-
-# k = The cost of the first banana
-# n = the initial dollar amount the soldier has 
-# w = the the number of bananas he wants 
-
-# Processing: 
-# Iterate over w to find out the total banana cost, by for looping the original k var and adding to a var each time, doubling this var 
-# Then, return the difference of n - total_cost
-
-
 initial_price, current_cash, desired_bananas = map(int, input().split())
 
 all_bananas = []
 
 for banana in range(1, desired_bananas + 1):
     all_bananas.append(banana * initial_price)
-    
-print(sum(all_bananas) - current_cash)
+
+if current_cash > sum(all_bananas):
+    print(0)
+else:
+    print(abs(sum(all_bananas) - current_cash))
 
