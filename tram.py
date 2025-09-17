2205ecebe6a4a812fcebc15c4fb01394cbeadd01
Stiven <Strnmrtnz@icloud.com>
2025-09-17 14:05:34 +0900

    feat: cleaned up solution for tram

--- tram.py
+++ tram.py
@@ -1,40 +1,16 @@
-# Input:
-# n = 1 to N number of train stops 
-# a = some passangers
-# b = some passangers 
-# train = starts empty at 1 station
-
-# Input Example:
-
-# 4 <- Number of train stops
-# 0 3 <- The N of people that exit, and the N of people that enter
-# Min Capacity Counter = 3
-# 2 5 <- The N of people that exit, and the N of people that enter 
-# So, 3 - 2 = 1 + 5 = 6, since we substract the min capacity counter from the the passangers exiting and then add the entering passangers, if the result is < min_counter, we must update min_counter to the latest minmax
-# 4 2
-# So, 6 - 4 = 2 + 2 = 4 
-# 4 0 
-# So, 4 - 4 = 0 + 0 = 0 
-
-# The relationship is, in iter 1, min_capacity_counter = current_line[1]
-# Afterwards min_capacity_counter = (min_capacity_counter - current_line[0]) + current_line[1]
-
 n = int(input())
 
-first_station = list(map(int,input().split()))
+current_passengers = 0
+max_passengers = 0
 
-min_counter = first_station[1]
+for station in range(n):
+    passengers_exit, passengers_enter = map(int, input().split())
+    
+    # Update current passenger count: subtract those who exit, add those who enter
+    current_passengers = current_passengers - passengers_exit + passengers_enter
+    
+    # Update maximum if current count is higher
+    if current_passengers > max_passengers:
+        max_passengers = current_passengers
 
-# Iter to n - 1 because we already inputted the first line 
-for station in range(n - 1):
-
-    # This will iterate over N remaining input lines
-    current_line = list(map(int,input().split()))
-
-    min_calc = (min_counter - current_line[0]) + current_line[1]
-
-    if min_calc > min_counter:
-        min_counter = min_calc
-
-print(min_counter)
-
+print(max_passengers)
