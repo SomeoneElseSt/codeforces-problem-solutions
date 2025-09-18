ea369eb4b09906c5af484482f35177d139b72dbc
Stiven <Strnmrtnz@icloud.com>
2025-09-18 23:54:38 +0900

    cleaned up, works but gives time limit exceeded on a test

--- calculating_function.py
+++ calculating_function.py
@@ -1,15 +1,18 @@
 # Input:
 # n = 1 <= n <= 10^25
 
-n = int(input())
-
 # Sequence:
-# - + - + - 
+# - + - + 
+
+# Processing 
+# We need to take the i-th element, and with the right operator modify it with the i-th + 1 element, keeping track of the result  
+
+n = int(input())
 
 result = 0
 switched = False
 
-for i in range(1, n):
+for i in range(1, n + 1):
 
     if switched == False:
         result -= i
