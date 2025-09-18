0bb4d47fa5848c775dc81ae5cc2661b74b620709
Stiven <Strnmrtnz@icloud.com>
2025-09-19 00:49:42 +0900

    getting there - learning gauss

--- calculating_function.py
+++ calculating_function.py
@@ -8,22 +8,30 @@
 # We need to take the i-th element, and with the right operator modify it with the i-th + 1 element, keeping track of the result  
 
 # The above was not efficient for a bigger N
-# The problem really is = sum(even numbers up to N) - sum(odd numbers up to N)
+# The problem really is = sum(even numbers up to N // 2) - sum(odd numbers up to N // 2)
 # So so long as I can get two lists each of n // 2 one even and one odd, this is trivial. Or two variables
 # The question is how to get them in O(1) or some other Theta-bound 
+# We can use Gauss's sum for this
 
-n = int(input())
+n = int(input()) 
 
-result = 0
-switched = False
+n_halved = n // 2
 
-for i in range(1, n + 1):
+def gauss_sum_even(n):
+    return 
 
-    if switched == False:
-        result -= i
-        switched = True
-    elif switched == True:
-        result += i
-        switched = False
+def gauss_sum_odd(n):
+    return 
 
-print(result)
+print(gauss_sum_even(n_halved) - gauss_sum_odd(n_halved))
+
+# Some innefficient implementation
+# for i in range(1, n + 1):
+
+#     if switched == False:
+#         result -= i
+#         switched = True
+#     elif switched == True:
+#         result += i
+#         switched = False
+# print(result)
