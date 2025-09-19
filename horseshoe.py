080909e1ef98d2334dbe48883e5bb9dfbd5f7308
Stiven <Strnmrtnz@icloud.com>
2025-09-20 03:12:23 +0900

    worked OK - left some comments

--- horseshoe.py
+++ horseshoe.py
@@ -1,5 +1,7 @@
 ints = list(map(int, input().split()))
 
+# Set de-dubs
 unique_ints = set(ints) 
 
+# The difference between the dedubbed list and the original is the amount of items that are duplicated
 print(len(ints) - len(unique_ints))
