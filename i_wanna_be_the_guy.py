0d62bdc442655870b0e59c9f89e65666f1daa79f
Stiven <Strnmrtnz@icloud.com>
2025-09-22 23:39:20 +0900

    first solution submission, let's see

---
+++ i_wanna_be_the_guy.py
@@ -0,0 +1,28 @@
+# Input:
+# N = Number of Levels
+# X = The level indices of the levels that X can pass
+# Y = The level indices of the levels that Y can pass
+
+# Rule:
+# X can only pass p levels. Y can only pass q levels. 
+
+# Processing:
+# First we'll want to use a set to dedub the input for both lists 
+# We'll also want to make a list in a range from 1 to N
+
+# Then we can compare using all in python if all the elements in the N list are also in the joined list of X, Y
+
+# Output: 
+# If they can pass all the levels, print "I become the guy." else print "Oh, my keyboard!"
+
+n = int(input())
+
+x = list(map(int,input().split()))
+y = list(map(int,input().split()))
+
+nums = set(x + y)
+levels = [i for i in range(1, n + 1)]
+
+result = "I become the guy" if all(int in nums for int in levels) else "Oh, my keyboard!"
+
+print(result)
