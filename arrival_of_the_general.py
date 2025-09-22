bb53cba28d4cbf09c262c2e3a3e4723ac898440d
Stiven <Strnmrtnz@icloud.com>
2025-09-23 01:08:35 +0900

    fixed indexing issue and accounted for double crossing

--- arrival_of_the_general.py
+++ arrival_of_the_general.py
@@ -14,10 +14,17 @@ max_val = max(i)
 max_i = i.index(max_val)
 
 min_val = min(i)
-min_i = (len(i) - 1 - i[::-1].index(min_val) + 1)
+min_i = len(i) - 1 - i[::-1].index(min_val)
 
 min_distance = n - min_i
 
-result = min_distance + max_i if n % 2 == 0 else min_distance + max_i - 1
+if n == 2:
+    result = 1 if i[0] < i[1] else 0
+elif max_i == 0 and (min_i - 1) == n - 1:
+    result = 0
+elif max_i > min_i:
+    result = (min_distance + max_i if n % 2 == 0 else min_distance + max_i - 1) - 1
+else: 
+    result = min_distance + max_i if n % 2 == 0 else min_distance + max_i - 1
 
 print(result)
