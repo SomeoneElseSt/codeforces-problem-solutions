dcdfde6c9d03ae3a60364647ce1b7dd887afa25f
Stiven <Strnmrtnz@icloud.com>
2025-09-23 01:12:35 +0900

    changed the list name to be more readable

--- arrival_of_the_general.py
+++ arrival_of_the_general.py
@@ -8,18 +8,18 @@
 # If the last i value is already the min, leave as is, otherwise swap with i - 1 value 
 
 n = int(input())
-i = list(map(int,input().split()))
+nums_list = list(map(int,input().split()))
 
-max_val = max(i)
-max_i = i.index(max_val)
+max_val = max(nums_list)
+max_i = nums_list.index(max_val)
 
-min_val = min(i)
-min_i = len(i) - 1 - i[::-1].index(min_val)
+min_val = min(nums_list)
+min_i = len(nums_list) - 1 - nums_list[::-1].index(min_val)
 
 min_distance = n - min_i
 
 if n == 2:
-    result = 1 if i[0] < i[1] else 0
+    result = 1 if nums_list[0] < [1] else 0
 elif max_i == 0 and (min_i - 1) == n - 1:
     result = 0
 elif max_i > min_i:
