d2710f3b01fd901f1959b4845231bce6e9eef8ff
Stiven <Strnmrtnz@icloud.com>
2025-09-25 13:42:27 +0900

    updated approach

--- games.py
+++ games.py
@@ -1,29 +1,18 @@
-# Input: 
-# n = number of matches 
-# h, a = number pairs of games
-
-# Processing: 
-# Two sets of numbers: 
-# Home Team, Guest Team 
-# If 
-
 n = int(input())
+
+teams_list = []
+for team in range(1, n+1):
+    teams_list.append(list(map(int, input().split())))
 
 counter = 0
 h1, a1 = 0, 0
 
-
-for team in range(1, n+1):
-    h2, a2 = map(int, input().split())
-
-    if a1 == h2:
+for team in teams_list:
+    if a1 == team[0]:
         counter += 1
-
-    h1, a1 = h2, a2
+    h1, a1 = team[0], team[1]
 
 print(counter)
 
-
-
-
+    
 
