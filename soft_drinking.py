8a8590d3f77ab369269f5d24ef0b1dfdcc278a2a
Stiven <Strnmrtnz@icloud.com>
2025-09-30 17:07:22 +0900

    feat: didn't notice nl, wrong formula. fixed

--- soft_drinking.py
+++ soft_drinking.py
@@ -6,6 +6,8 @@ l = the ml of the drink in each bottle
 c = the # of limes
 d = the # of slices they made with c
 p = the grams of salt they have
+nl = the milimeters per friend
+np = the amount of salt per friend 
 
 For each friend, to make a toast they require nl of the drink and np grams of salt
 
@@ -16,8 +18,8 @@ List all the constraints, pick smallest one and divide it by the number of peopl
 
 n, k, l, c, d, p, nl, np = map(int, input().split())
 
-constraint_m = k * l // n
-constraint_l = c * d
-constraint_s = p // np
+constraint_m = k * l // nl # Avail. mils // needed mils
+constraint_l = c * d # Avail. slices
+constraint_s = p // np # Avail. salt // needed salt
 
-print(min(constraint_m, constraint_l, constraint_s) // n)
+print(min(constraint_m, constraint_l, constraint_s) // n) 
