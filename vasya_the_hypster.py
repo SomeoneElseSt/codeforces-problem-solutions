ee050065894916221167b93c335fe4ac8a01010a
Stiven <Strnmrtnz@icloud.com>
2025-10-08 23:59:22 +0900

    feat: solution to vasya the hipster, unoptimized

---
+++ vasya_the_hypster.py
@@ -0,0 +1,29 @@
+# Two pairs of numbers, a, b
+# How many times can you -= a, b before 
+# A == 0 and A != 0 or B == 0 and A != 0 
+# Then, if the resulting A, B > 1
+# return times from above + resulting A, B - 1
+
+a, b = map(int, input().split())
+T_COUNTER = 0
+I_COUNTER = 0
+
+while a > 0 or b > 0:
+    if a == 0:
+        if b == 1:
+            break
+        else:
+            b -= 2
+            I_COUNTER += 1
+    elif b == 0:
+        if a == 1:
+            break
+        else:
+            a -= 2
+            I_COUNTER += 1
+    else:
+        a -= 1
+        b -= 1
+        T_COUNTER += 1
+
+print(T_COUNTER, I_COUNTER)
