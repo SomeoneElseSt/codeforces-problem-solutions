e89f76ba531a06e3d7d25837716b27baf0ecf629
Stiven <Strnmrtnz@icloud.com>
2025-10-23 00:29:49 +0900

    feat: updated spy to be more efficient for small N

--- spy_detected.py
+++ spy_detected.py
@@ -1,15 +1,25 @@
 # Make a set of the elements
-# The spy is whichever's count 1 
 # Get the index of the spy
+# The spy is whichever's count 1 
 
 n = int(input())
 
 for i in range(n):
     c_test_l = int(input())
     c_test = list(input().split())
-    a, b = set(c_test)
 
-    if c_test.count(a) == 1:
-        print(c_test.index(a) + 1)
+    # If the first element isn't equal to the 1st, 2nd, it's the spy
+    if c_test[0] != c_test[1] and c_test[0] != c_test[2]:
+        print(1)
+        continue
+    # If the second element isn't equal to the 1st, 2nd, it's the spy
+    elif c_test[1] != c_test[0] and c_test[1] != c_test[2]:
+        print(2)
+        continue
+    # Otherwise it's true the first element is not a spy and can be used for comparison
     else:
-        print(c_test.index(b) + 1)
+        n_spy = c_test[0]
+        for i in range(2, len(c_test)):
+            if c_test[i] != n_spy:
+                print(i + 1)
+                break
