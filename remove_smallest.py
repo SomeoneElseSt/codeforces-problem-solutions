b250fb26d7bb663a1f905b174142c934402cf25c
Stiven <Strnmrtnz@icloud.com>
2025-10-24 07:26:05 +0900

    feat: solution to remove smallest using a set

---
+++ remove_smallest.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+for i in range(n):
+    c_len = int(input())
+    c_test = list(map(int,input().split()))
+
+    if len(c_test) == 1:
+        print("YES")
+        continue
+    else:
+        # Heuristic: if diff between 2 nums is > 1, it's a no
+        # Set -> diff for i-th, i-th+1 > 1? No! else Yes!
+        temp = sorted(list(set(c_test)))
+
+        for j in range(0, len(temp) - 1):
+            diff = abs(temp[j] - temp[j+1])
+            if diff > 1:
+                print("NO")
+                break
+        else: # if break != true
+            print("YES")
+    
