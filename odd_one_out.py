233810e9acc4f8eb4884aa576d919e871925b3ac
Stiven <Strnmrtnz@icloud.com>
2025-10-25 20:07:15 +0900

    feat: solution to odd one out

---
+++ odd_one_out.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n): 
+    a, b, c = map(int, input().split())
+
+    if a != b and a != c:
+        print(a)
+    elif b != a and b != c:
+        print(b)
+    elif c != a and c != b:
+        print(c)
+
+
+
