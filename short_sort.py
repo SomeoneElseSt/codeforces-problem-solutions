f818bfbb6ea0d80dc9e66375a8959db481fd1c91
Stiven <Strnmrtnz@icloud.com>
2025-11-03 08:00:16 +0900

    feat: cleaned up code

--- short_sort.py
+++ short_sort.py
@@ -10,7 +10,6 @@ for t in range(n):
         c_t_a = c_t.copy()
         c_t_b = c_t.copy()
         c_t_c = c_t.copy()
-        t = False
 
         c_t_a[0], c_t_a[1] = c_t_a[1], c_t_a[0]
         c_t_b[1], c_t_b[2] = c_t_b[2], c_t_b[1]
@@ -18,8 +17,6 @@ for t in range(n):
 
         if "".join(c_t_a) == 'abc' or "".join(c_t_b) == 'abc' or "".join(c_t_c) == 'abc':
             print("YES")
-            t = True
             continue
 
-    if not t:
-        print("NO")
+    print("NO")
