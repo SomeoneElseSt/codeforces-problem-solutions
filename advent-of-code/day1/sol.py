e880d607f28e6a2a5f0bfcfd0dc7e659f8735382
Stiven <Strnmrtnz@icloud.com>
2025-12-02 15:29:46 +0900

    fix: realized input has got < 99 vals - breaking them down into chunks - need to add iteration

--- advent-of-code/day1/sol.py
+++ advent-of-code/day1/sol.py
@@ -16,41 +16,56 @@ COUNTER = 0
  
 # Iterate over each line 
 for line in input:
-    # Determine the direction extracting L/R with a regex
-    if "L" in line:
-        line_n = int(re.sub("L", "", line))
-        # print(f"current line {line_n}")
+    
+    # For > 100 rotations they have to be broken up in < 99 chunks
+    strpd_line = int(re.sub("[^0-9]", "", line))
+
+    # Re-append < 99 chunks with R/L rotation marker 
+    if strpd_line > 100:
+        lines = []
+
+        while strpd_line > 0:
+       
+    else:
+        lines = [line]
+
+    for l in lines: 
+
+        # Determine the direction extracting L/R with a regex
+        if "L" in l:
+            line_n = int(re.sub("L", "", line))
+            print(f"current line {line_n}")
 
 
-        if DIAL - line_n < 0: 
-            DIAL -= (line_n - 100)
-            # print(f"current line was LEFT and < 0. DIAL {DIAL}\n")
+            if DIAL - line_n < 0: 
+                DIAL -= (line_n - 100)
+                print(f"current line was LEFT and < 0. DIAL {DIAL}\n")
 
-        elif DIAL - line_n > 0:
-            DIAL -= line_n
-            # print(f"current line was LEFT and > 0. DIAL {DIAL}\n")
+            elif DIAL - line_n > 0:
+                DIAL -= line_n
+                print(f"current line was LEFT and > 0. DIAL {DIAL}\n")
 
-        elif DIAL - line_n == 0:
-            DIAL = 0
-            COUNTER += 1
-            # print(f"current line was LEFT and == 0. DIAL {DIAL}\n")
-        
-    elif "R" in line:
-        line_n = int(re.sub("R", "", line))
-        # print(f"current line {line_n}")
-
-        # The only way to go over is if the sum is > 99      
-        if DIAL + line_n > 99:
-            if (DIAL + line_n) - 100 == 0:
+            elif DIAL - line_n == 0:
                 DIAL = 0
                 COUNTER += 1
-                # print(f"current line was RIGHT and == 0. DIAL {DIAL}\n")
-            else:
-                DIAL = (DIAL + line_n) - 100
-                # print(f"current line was RIGHT and > 99. DIAL {DIAL}\n")
+                print(f"current line was LEFT and == 0. DIAL {DIAL}\n")
+            
+        elif "R" in l:
+            line_n = int(re.sub("R", "", line))
+            print(f"current line {line_n}")
 
-        elif DIAL + line_n < 99:
-            DIAL += line_n
-            # print(f"current line was RIGHT and < 99. DIAL {DIAL}\n")
+            # The only way to go over is if the sum is > 99      
+            if DIAL + line_n > 99:
+                if (DIAL + line_n) - 100 == 0:
+                    DIAL = 0
+                    COUNTER += 1
+                    print(f"current line was RIGHT and == 0. DIAL {DIAL}\n")
+                else:
+                    DIAL = (DIAL + line_n) - 100
+                    print(f"current line was RIGHT and > 99. DIAL {DIAL}\n")
+
+            elif DIAL + line_n < 99:
+                DIAL += line_n
+                print(f"current line was RIGHT and < 99. DIAL {DIAL}\n")
 
 print(f"counter {COUNTER}")
