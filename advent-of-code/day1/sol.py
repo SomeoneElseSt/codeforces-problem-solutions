cff353ce174db0701a4fd4fd758964b2cc0465db
Stiven <Strnmrtnz@icloud.com>
2025-12-02 19:17:23 +0900

    fix: fixed wrap around logic using while loops

--- advent-of-code/day1/sol.py
+++ advent-of-code/day1/sol.py
@@ -44,36 +44,23 @@ for line in input:
             line_n = int(re.sub("L", "", l))
             print(f"current line {line_n}")
 
-            if DIAL - line_n < 0: 
-                DIAL -= (line_n - 100)
-                print(f"current line was LEFT and < 0. DIAL {DIAL}\n")
-
-            elif DIAL - line_n > 0:
-                DIAL -= line_n
-                print(f"current line was LEFT and > 0. DIAL {DIAL}\n")
-
-            elif DIAL - line_n == 0:
-                DIAL = 0
-                COUNTER += 1
-                print(f"current line was LEFT and == 0. DIAL {DIAL}\n")
+            DIAL -= line_n
+            while DIAL < 0:
+                DIAL += 100
+            print(f"current line was LEFT. DIAL {DIAL}\n")
             
         elif "R" in l:
             line_n = int(re.sub("R", "", l))
             print(f"current line {line_n}")
 
-            # The only way to go over is if the sum is > 99      
-            if DIAL + line_n > 99:
-                if (DIAL + line_n) - 100 == 0:
-                    DIAL = 0
-                    COUNTER += 1
-                    print(f"current line was RIGHT and == 0. DIAL {DIAL}\n")
-                else:
-                    DIAL = (DIAL + line_n) - 100
-                    print(f"current line was RIGHT and > 99. DIAL {DIAL}\n")
+            DIAL += line_n
+            while DIAL > 99:
+                DIAL -= 100
+            print(f"current line was RIGHT. DIAL {DIAL}\n")
 
-            elif DIAL + line_n <= 99:
-                DIAL += line_n
-                print(f"current line was RIGHT and < 99. DIAL {DIAL}\n")
+    if DIAL == 0:
+        COUNTER += 1
+        print(f">>> LANDED ON 0! Counter: {COUNTER}\n")
 
 print()
 print(f"counter {COUNTER}")
