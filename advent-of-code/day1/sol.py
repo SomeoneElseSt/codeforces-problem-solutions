99415adebec3146f9b44018c6ebbd0b10f2e91f4
Stiven <Strnmrtnz@icloud.com>
2025-12-02 12:33:05 +0900

    chore: added debug print lines

--- advent-of-code/day1/sol.py
+++ advent-of-code/day1/sol.py
@@ -9,7 +9,7 @@
 import re
 
 # Read input.txt 
-input = open("test.txt")
+input = open("input.txt")
 
 DIAL = 50
 COUNTER = 0
@@ -19,35 +19,38 @@ for line in input:
     # Determine the direction extracting L/R with a regex
     if "L" in line:
         line_n = int(re.sub("L", "", line))
+        # print(f"current line {line_n}")
+
 
         if DIAL - line_n < 0: 
             DIAL -= (line_n - 100)
+            # print(f"current line was LEFT and < 0. DIAL {DIAL}\n")
 
         elif DIAL - line_n > 0:
             DIAL -= line_n
+            # print(f"current line was LEFT and > 0. DIAL {DIAL}\n")
 
         elif DIAL - line_n == 0:
             DIAL = 0
             COUNTER += 1
-
-
+            # print(f"current line was LEFT and == 0. DIAL {DIAL}\n")
+        
     elif "R" in line:
         line_n = int(re.sub("R", "", line))
+        # print(f"current line {line_n}")
 
         # The only way to go over is if the sum is > 99      
         if DIAL + line_n > 99:
             if (DIAL + line_n) - 100 == 0:
                 DIAL = 0
                 COUNTER += 1
+                # print(f"current line was RIGHT and == 0. DIAL {DIAL}\n")
             else:
                 DIAL = (DIAL + line_n) - 100
+                # print(f"current line was RIGHT and > 99. DIAL {DIAL}\n")
 
-    elif DIAL + line_n < 99:
-        DIAL += line_n
+        elif DIAL + line_n < 99:
+            DIAL += line_n
+            # print(f"current line was RIGHT and < 99. DIAL {DIAL}\n")
 
-print(COUNTER)
-
-        
-
-
-
+print(f"counter {COUNTER}")
